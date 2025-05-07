766506501cb66539f0fda7ab8a311a0efb4941cf
Hariyah <65937599+hariyah@users.noreply.github.com>
2025-05-07 16:56:05 +0530

    Delete frontend react directory

--- frontend react/src/Store/Auth/Selectors.js
+++
@@ -1,8 +0,0 @@
-// Selectors for auth state
-export const selectAuth = (state) => state.auth;
-export const selectUser = (state) => state.auth.user;
-export const selectAuthLoading = (state) => state.auth.loading;
-export const selectAuthError = (state) => state.auth.error;
-export const selectJwt = (state) => state.auth.jwt;
-export const selectUpdateUser = (state) => state.auth.updateUser;
-export const selectSearchResult = (state) => state.auth.searchResult; 
